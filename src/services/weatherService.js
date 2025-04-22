2884ee6671bd202038a879997042153b0cd8fbb8
Nazanin Hashemian <69569381+NazaninHashemian@users.noreply.github.com>
2025-04-22 14:57:49 -0700

    Remove weather fromm baseURL to make API more general, not only specified to weather

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -5,14 +5,15 @@ import axios from 'axios';
 const apiKey = '4e5c6111439b8ec97661a32222b32c21';
 
 const http = axios.create({
-    baseURL: 'https://api.openweathermap.org/data/2.5/weather'
+    baseURL: 'https://api.openweathermap.org/data/2.5/'
 });
 
 const fetchWeather = async (city) => {
     try {
-        const response = await http
-            .get(`?q=${city}&appid=${apiKey}`);
-        return response.data;
+        // Get current weather data
+        const currentWeatherResponse = await http.get(
+            `weather?q=${city}&appid=${apiKey}`);
+        return currentWeatherResponse.data;
     } catch (error) {
         // Propagate error for handling in the component
         throw error;
