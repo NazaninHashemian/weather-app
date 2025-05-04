520dde07f79da9dd7b1d1072b516706c6d19d28a
Nazanin Hashemian <69569381+NazaninHashemian@users.noreply.github.com>
2025-05-04 00:55:06 -0700

    refactor: remove inline WeatherData and Condition interfaces and import them from weatherTypes

--- src/components/Weather/Weather.tsx
+++ src/components/Weather/Weather.tsx
@@ -10,47 +10,47 @@ import './Weather.css';
 import SearchBar from '../SearchBar/SearchBar.js';
 import WeatherCard from '../WeatherCard/WeatherCard.js';
 import ExtraInfoCard from '../ExtraInfoCard/ExtraInfoCard.js';
-
+import {WeatherData, Condition} from '../../types/weatherTypes.js'
 
 // type Unit = 'Celsius' | 'Fahrenheit' | 'Kelvin';
 
-interface Condition {
-  text: string;
-  icon: string;
-}
+// interface Condition {
+//   text: string;
+//   icon: string;
+// }
 
-interface WeatherData {
-  location: {
-    name: string;
-    lat: number;
-    lon: number;
-  };
-  current: {
-    temp_c: number;
-    feelslike_c: number;
-    humidity: number;
-    wind_kph: number;
-    condition: Condition;
-  };
-  forecast: {
-    forecastday: Array<{
-      day: {
-        maxtemp_c: number;
-        mintemp_c: number;
-        daily_chance_of_rain: number;
-      };
-      astro: {
-        sunrise: string;
-        sunset: string;
-      };
-      hour: Array<{
-        time: string;
-        temp_c: number;
-        condition: Condition;
-      }>;
-    }>;
-  };
-}
+// interface WeatherData {
+//   location: {
+//     name: string;
+//     lat: number;
+//     lon: number;
+//   };
+//   current: {
+//     temp_c: number;
+//     feelslike_c: number;
+//     humidity: number;
+//     wind_kph: number;
+//     condition: Condition;
+//   };
+//   forecast: {
+//     forecastday: Array<{
+//       day: {
+//         maxtemp_c: number;
+//         mintemp_c: number;
+//         daily_chance_of_rain: number;
+//       };
+//       astro: {
+//         sunrise: string;
+//         sunset: string;
+//       };
+//       hour: Array<{
+//         time: string;
+//         temp_c: number;
+//         condition: Condition;
+//       }>;
+//     }>;
+//   };
+// }
 
 function Weather() {
   const [city, setCity] = useState('Coquitlam');
