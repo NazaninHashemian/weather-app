ce76506addd61750140075a0d55dc8324cd1df17
Nazanin Hashemian <69569381+NazaninHashemian@users.noreply.github.com>
2025-05-04 01:05:21 -0700

    Refactor Weather component to integrate HourlyForecast component with props for icon and temperature

---
+++ src/components/HourlyForecast/HourlyForecast.tsx
@@ -0,0 +1,35 @@
+import React, { ReactNode } from 'react'
+import { Condition, WeatherData } from '../../types/weatherTypes';
+
+interface HourlyForecastProps {
+    weatherData: WeatherData;
+    unitSymbols: { unit: string };
+    onGetWeatherIcon: (value: Condition | undefined) => ReactNode;
+    onGetTemperature: (value: number) => string;
+}
+
+function HourlyForecast({weatherData, unitSymbols , onGetWeatherIcon, onGetTemperature}: HourlyForecastProps) {
+  return (
+    <>
+        <div className="gradient-line"></div>
+            <div  className="hourly-forecast-container">
+                <h2>Hourly Forecast</h2>
+                <div className="hourly-forecast">
+                  {weatherData.forecast.forecastday[0].hour.map((hour, index) => (
+                    <div key={index} className="hour">
+                      {/* <p>{hour.time.split(' ')[1]}</p> */}
+                      <p>{new Date(hour.time).toLocaleTimeString([], { hour: 'numeric', hour12: true })}</p>
+                      {onGetWeatherIcon(hour.condition)}
+                      <p>{hour.condition.text}</p>
+                      <p>
+                        {onGetTemperature(hour.temp_c)}°{unitSymbols.unit}
+                      </p>
+                    </div>
+                  ))}
+                </div>
+            </div>
+        </>
+  )
+}
+
+export default HourlyForecast
--- src/components/Weather/Weather.tsx
+++ src/components/Weather/Weather.tsx
@@ -11,6 +11,7 @@ import SearchBar from '../SearchBar/SearchBar.js';
 import WeatherCard from '../WeatherCard/WeatherCard.js';
 import ExtraInfoCard from '../ExtraInfoCard/ExtraInfoCard.js';
 import {WeatherData, Condition} from '../../types/weatherTypes.js'
+import HourlyForecast from '../HourlyForecast/HourlyForecast.js';
 
 // type Unit = 'Celsius' | 'Fahrenheit' | 'Kelvin';
 
@@ -196,25 +197,33 @@ function Weather() {
         {/* <hr className="divider" /> */}
 
         {weatherData && (
-          <>
-            <div className="gradient-line"></div>
-            <div  className="hourly-forecast-container">
-                <h2>Hourly Forecast</h2>
-                <div className="hourly-forecast">
-                  {weatherData.forecast.forecastday[0].hour.map((hour, index) => (
-                    <div key={index} className="hour">
-                      {/* <p>{hour.time.split(' ')[1]}</p> */}
-                      <p>{new Date(hour.time).toLocaleTimeString([], { hour: 'numeric', hour12: true })}</p>
-                      {getWeatherIcon(hour.condition)}
-                      <p>{hour.condition.text}</p>
-                      <p>
-                        {getTemperature(hour.temp_c)}°{unitSymbols[unit]}
-                      </p>
-                    </div>
-                  ))}
-                </div>
-            </div>
-          </>
+          <HourlyForecast 
+            weatherData={weatherData} 
+            unitSymbols={{
+            unit: unit
+            }} 
+            onGetWeatherIcon={getWeatherIcon} 
+            onGetTemperature={getTemperature} 
+        />
+          // <>
+          //   <div className="gradient-line"></div>
+          //   <div  className="hourly-forecast-container">
+          //       <h2>Hourly Forecast</h2>
+          //       <div className="hourly-forecast">
+          //         {weatherData.forecast.forecastday[0].hour.map((hour, index) => (
+          //           <div key={index} className="hour">
+          //             {/* <p>{hour.time.split(' ')[1]}</p> */}
+          //             <p>{new Date(hour.time).toLocaleTimeString([], { hour: 'numeric', hour12: true })}</p>
+          //             {getWeatherIcon(hour.condition)}
+          //             <p>{hour.condition.text}</p>
+          //             <p>
+          //               {getTemperature(hour.temp_c)}°{unitSymbols[unit]}
+          //             </p>
+          //           </div>
+          //         ))}
+          //       </div>
+          //   </div>
+          // </>
         )}
       </div>
     
