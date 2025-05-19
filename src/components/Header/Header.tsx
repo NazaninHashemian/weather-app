72e03427f23e4061e86f58d46ab2dae9b1bc4e20
Nazanin Hashemian <69569381+NazaninHashemian@users.noreply.github.com>
2025-05-18 22:40:20 -0700

    Fix layout shift in header by preserving icon space when weather data is missing

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -10,11 +10,9 @@ function Header({ weatherData }: HeaderProps) {
   return (
     <div className="header">
       <div className="header-content">
-        {weatherData && (
-          <div className="weather-icon">
-            {getWeatherIcon(weatherData.current.condition, 'large')}
-          </div>
-        )}
+        <div className="weather-icon">
+          {weatherData ? getWeatherIcon(weatherData.current.condition, 'large') : null}
+        </div>
         <h1>Weather Forecast</h1>
     </div>
   </div>
