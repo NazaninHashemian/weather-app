6855d0ac8d6ca0658a5a2c9e777d8b7d59312ac8
Nazanin Hashemian <69569381+NazaninHashemian@users.noreply.github.com>
2025-05-18 22:25:00 -0700

    Create Header.css

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import getWeatherIcon from '../../utils/getWeatherIcon'
 import { WeatherData } from '../../types/weatherTypes'
+import './Header.css'
 
 interface HeaderProps {
     weatherData: WeatherData | null;
@@ -8,13 +9,13 @@ interface HeaderProps {
 function Header({ weatherData }: HeaderProps) {
   return (
     <div className="header">
-    <div className="header-content">
-      {weatherData && (
-        <div className="weather-icon">
-          {getWeatherIcon(weatherData.current.condition, 'large')}
-        </div>
-      )}
-      <h1>Weather Forecast</h1>
+      <div className="header-content">
+        {weatherData && (
+          <div className="weather-icon">
+            {getWeatherIcon(weatherData.current.condition, 'large')}
+          </div>
+        )}
+        <h1>Weather Forecast</h1>
     </div>
   </div>
   )
